7fedd4f62d72b0cbf83aa618940c4a2731446844
remoo1901 <remoo1901@hotmail.com>
2020-04-25 14:21:54 -0400

    finished styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,39 @@
-import React from 'react';
-import "./App.css"
-import DarkMode from "./components/DarkMode"
-import PlayerCard from "./components/PlayerCard"
-import axios from "axios"
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import PlayerCard from "./components/PlayerCard";
+import DarkMode from "./components/DarkMode";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       data: []
-    }
-  }
-    componentDidMount(){
-      axios
-           .get("http://localhost:5000/api/players")
-           .then(response => {this.setState({data: response.data})})
-           .catch(err => {
-             console.log("ERROR", err)
-            })
-    }
- 
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Women's World Cup</h1>
-        <DarkMode />
-        <PlayerCard data={this.state.data}/> 
-      </header>
-    </div>
-  );
+    };
   }
 
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(response => {
+        this.setState({ data: response.data });
+      })
+      .catch(err => {
+        console.log("ERROR", err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Women's World Cup</h1>
+        </header>
+        <DarkMode />
+        <PlayerCard data={this.state.data} />
+      </div>
+    );
+  }
 }
+
 export default App;
--- client/src/components/DarkMode.js
+++ client/src/components/DarkMode.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { useDarkMode } from "../hooks/useDarkMode";
 
+
 const DarkMode = () => {
   const [darkMode, setDarkMode] = useDarkMode(true);
-  const toggleMode = (e) => {
+  const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
-
+  
   return (
-    <div>
-      <div
-        onClick={toggleMode}
-        className={darkMode ? "toggle toggled" : "toggle"}
-      />
-    </div>
+    
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? "toggle toggled" : "toggle"}
+        />
+      </div>
+   
   );
 };
 
